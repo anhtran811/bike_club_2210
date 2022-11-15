1279d2b4ff7f9b34186240fae1962e17183d01d8
Anhnhi Tran <anhtran811@gmail.com>
2022-11-15 09:23:12 -0700

    Wrote log rides test

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -3,6 +3,8 @@ require './lib/biker'
 
 RSpec.describe Biker do
   let!(:biker) {Biker.new("Kenny", 30)}
+  let!(:ride1) {Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})}
+  let!(:ride1) {Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})}
 
   it 'exists and has attributes' do
     expect(biker).to be_a(Biker)
@@ -15,21 +17,27 @@ RSpec.describe Biker do
   it 'can learn terrain' do
     biker.learn_terrain!(:gravel)
     biker.learn_terrain!(:hills)
-
+    
     expect(biker.acceptable_terrain).to eq([:gravel, :hills])
+  end
+  
+  it 'can log rides' do
+    biker.learn_terrain!(:gravel)
+    biker.learn_terrain!(:hills)
+
+    biker.log_ride(ride1, 92.5)
+    biker.log_ride(ride1, 91.1)
+    biker.log_ride(ride2, 60.9)
+    biker.log_ride(ride2, 61.6)
+
+    expect(biker.rides).to eq({
+      ride1 => [92.5, 91.1]
+      ride2 => [60.9, 61.6]
+    })
   end
 
 end
 
-
-# biker.acceptable_terrain
-# => [:gravel, :hills]
-
-# ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
-# => #<Ride:0x00007fc62ca32a10...>
-
-# ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
-# => #<Ride:0x00007fc62cb42ba8...>
 
 # biker.log_ride(ride1, 92.5)
 
