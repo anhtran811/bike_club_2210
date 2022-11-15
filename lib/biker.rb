30ae48229dc730eaee4b9f8de0d14294d1ce4c83
Anhnhi Tran <anhtran811@gmail.com>
2022-11-15 09:31:28 -0700

    Create new ride instance and ride requirements test

--- lib/biker.rb
+++ lib/biker.rb
@@ -15,5 +15,11 @@ class Biker
     @acceptable_terrain << terrain
   end
 
- 
+  def log_ride(ride, time)
+    if @rides[ride].nil? 
+      @rides[ride] = [time]
+    else
+      @rides[ride] << time
+    end
+  end
 end
--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Biker do
   let!(:biker) {Biker.new("Kenny", 30)}
   let!(:ride1) {Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})}
   let!(:ride2) {Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})}
+  let!(:ride3) {Ride.new({name: "Coconut Lake", distance: 32, loop: true, terrain: :gravel})}
 
   it 'exists and has attributes' do
     expect(biker).to be_a(Biker)
@@ -36,22 +37,19 @@ RSpec.describe Biker do
     })
   end
 
+  it 'will ride based on terrain and distance requirements' do
+    biker.learn_terrain!(:gravel)
+    biker.log_ride(ride1, 92.5)
+    biker.log_ride(ride2, 60.9)
+    biker.log_ride(ride3, 80.0)
+
+    expect(biker.rides).to eq({
+      ride2 => [60.9]
+    })
+  end
+
 end
 
-
-# biker.log_ride(ride1, 92.5)
-
-# biker.log_ride(ride1, 91.1)
-
-# biker.log_ride(ride2, 60.9)
-
-# biker.log_ride(ride2, 61.6)
-
-# biker.rides
-# => {
-#      #<Ride:0x00007fc62ca32a10...> => [92.5, 91.1],
-#      #<Ride:0x00007fc62cb42ba8...> => [60.9, 61.6]
-#    }
 
 # # biker.personal_record(ride1)
 # => 91.1
